541155005f8b948ec10b91e33ff63a8829f3ddf5
Saint <ss.mscx@gmail.com>
2026-06-19 23:54:29 -0600

    funcionamiento de la alarma

--- alarm_clock.py
+++ alarm_clock.py
@@ -1,6 +1,9 @@
 from playsound import playsound
 import time
 
+CLEAR = "\033[H"
+CLEAR_AND_RETURN = "\033[H"
+
 def alarm(seconds):
     time_elapsed = 0
 
@@ -12,7 +15,17 @@ def alarm(seconds):
         time_left = seconds - time_elapsed
         minutes_left = time_left // 60
         seconds_left = time_left % 60
+        # 02d es para mostrar siempre dos dígitos, incluso si el número es menor a 10
+        print(f"{CLEAR_AND_RETURN}Tiempo restante: {minutes_left:02d} minutos y {seconds_left:02d} segundos")
 
-        print(f"Tiempo restante: {minutes_left} minutos y {seconds_left} segundos")
+    playsound("alarm.mp3")
 
-alarm(10)
+minutes = int(input("Ingrese el número de minutos para la alarma: "))
+seconds = int(input("Ingrese el número de segundos para la alarma: "))
+total_seconds = minutes * 60 + seconds
+alarm(total_seconds)
+
+
+# activar el entrono virtual
+# .\venv\Scripts\Activate.ps1
+# venv\Scripts\activate
